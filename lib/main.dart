e24b07f5cab35fc935c4e6b50fb9202193096dc3
[shun-kon] <[kondo.s.ak2@gmail.com]>
2023-11-29 05:00:11 +0900

    Step 4: Implement the text section

--- lib/main.dart
+++ lib/main.dart
@@ -74,6 +74,19 @@ Widget buttonSection = Row(
   ],
 );
 
+Widget textSection = Container(
+  padding: const EdgeInsets.all(32),
+  child: const Text(
+    'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+    'Alps. Situated 1,578 meters above sea level, it is one of the '
+    'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+    'half-hour walk through pastures and pine forest, leads you to the '
+    'lake, which warms to 20 degrees Celsius in the summer. Activities '
+    'enjoyed here include rowing, and riding the summer toboggan run.',
+    softWrap: true,
+  ),
+);
+
     return MaterialApp(
       title: 'Flutter layout demo',
       home: Scaffold(
@@ -84,6 +97,7 @@ Widget buttonSection = Row(
             children:[
                titleSection,
                buttonSection,
+               textSection,
             ]
             )
         ),
